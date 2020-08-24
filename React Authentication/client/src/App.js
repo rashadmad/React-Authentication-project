75405c42b55f8055f77a331c9afd9e03ecd665d1
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-08-24 13:38:46 -0700

    initial commit

---
+++ React Authentication/client/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+import Header from './components/Header';
+import Public from './components/Public';
+import NotFound from './components/NotFound';
+import UserSignUp from './components/UserSignUp';
+import UserSignIn from './components/UserSignIn';
+import UserSignOut from './components/UserSignOut';
+import Authenticated from './components/Authenticated';
+ // New import
+ import withContext from './Context';
+
+ const UserSignUpWithContext = withContext(UserSignUp);
+ // connect UserSignIn to context
+ const UserSignInWithContext = withContext(UserSignIn);  
+
+export default () => (
+  <Router>
+    <div>
+      <Header />
+
+      <Switch>
+        <Route exact path="/" component={Public} />
+        <Route path="/authenticated" component={Authenticated} />
+        <Route path="/signin" component={UserSignInWithContext} />
+        <Route path="/signup" component={UserSignUpWithContext} />
+        <Route path="/signout" component={UserSignOut} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </Router>
+);
